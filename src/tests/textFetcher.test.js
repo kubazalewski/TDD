5862c82469490830da4aebb6afc20ff84efb6e2a
kubazalewski <jakub.zalewski@netguru.pl>
2019-02-09 15:44:30 +0100

    write failing test for RandomText component

--- src/tests/textFetcher.test.js
+++ src/tests/textFetcher.test.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import { render } from 'react-testing-library';
 
+test('Pass props to randomText and display a text', () => {
+
+  const { getByTestId } = render(
+    <RandomText text="Just a random text to make test fail"/>
+  )
+
+  expect(getByTestId('test-text').toHaveTextContent(
+    "Just a random text to make test fail"
+  ))
+})

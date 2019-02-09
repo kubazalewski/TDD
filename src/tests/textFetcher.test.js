27816815782c71c6b3f4dd28fed7665288df8a5f
kubazalewski <jakub.zalewski@netguru.pl>
2019-02-09 16:09:23 +0100

    write failing test for TextFetcher

--- src/tests/textFetcher.test.js
+++ src/tests/textFetcher.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-testing-library';
 import 'jest-dom/extend-expect'
 import RandomText from '../RandomText'
+import TextFetcher from '../TextFetcher'
 
 test('Pass props to randomText and display a text', () => {
 
@@ -12,4 +13,13 @@ test('Pass props to randomText and display a text', () => {
   expect(getByTestId('test-text')).toHaveTextContent(
     "Just a random text to make test fail"
   )
+});
+
+test('TextFetcher fetches a random text and passes to RandomText as a prop to display it', () => {
+
+  const { getByText } = render(
+    <TextFetcher />
+  )
+
+  expect(getByText('Please load some text first')).toBeInTheDOM()
 })

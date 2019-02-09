a77326a8a9b7d26a5a614889fd8aac676a58276c
kubazalewski <jakub.zalewski@netguru.pl>
2019-02-09 16:39:16 +0100

    write failing test for clicking fetch text button

--- src/tests/textFetcher.test.js
+++ src/tests/textFetcher.test.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { render } from 'react-testing-library';
-import 'jest-dom/extend-expect'
-import RandomText from '../RandomText'
-import TextFetcher from '../TextFetcher'
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import RandomText from '../RandomText';
+import TextFetcher from '../TextFetcher';
 
 test('Pass props to randomText and display a text', () => {
-
   const { getByTestId } = render(
-    <RandomText text="Just a random text to make test fail"/>
-  )
+    <RandomText text="Just a random text to make test fail" />
+  );
 
   expect(getByTestId('test-text')).toHaveTextContent(
-    "Just a random text to make test fail"
-  )
+    'Just a random text to make test fail'
+  );
 });
 
 test('TextFetcher fetches a random text and passes to RandomText as a prop to display it', () => {
+  const { getByText } = render(<TextFetcher />);
 
-  const { getByText } = render(
-    <TextFetcher />
-  )
+  expect(getByText('Please load some text first')).toBeInTheDOM();
 
-  expect(getByText('Please load some text first')).toBeInTheDOM()
-})
+  fireEvent.click(getByText('Fetch random text'));
+
+  expect(queryByText('Please load some text first')).not.toBeInTheDOM();
+});
